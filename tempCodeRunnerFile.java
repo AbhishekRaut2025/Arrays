554ccccbbc14bc1567d4ea7333164fde748733cc
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-07-06 23:37:45 +0530

    temp file

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,6 @@
+int firstElement = numbers[0];
+        // int d = 2;
+        // for (int l = 0; l <= d; l++) {
+        //     numbers[n - 1] = firstElement;
+        // }
+        // System.out.println("The left rotated array by "+d+" places : ");
